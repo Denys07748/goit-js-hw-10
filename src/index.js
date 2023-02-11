e09a44603331aa5aee8392d7eede96875d4e07be
Denys07748 <denis07748@gmail.com>
2023-02-11 17:36:51 +0200

    додає стартові файли

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+// import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchCountry: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchCountry.addEventListener(
+  'input',
+  debounce(onSearch, DEBOUNCE_DELAY)
+);
+
+function onSearch() {
+  console.log(refs.searchCountry.value);
+}

70106a4ffe64d0e3cc0d5f1c5b4d978282933ad9
Denys07748 <denis07748@gmail.com>
2023-02-11 22:39:43 +0200

    чернова реалізація

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+export function fetchCountries(country) {
+  return fetch(
+    `${BASE_URL}/name/${country}?fields=name,capital,population,languages,flags`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import './css/styles.css';
-// import debounce from 'lodash.debounce';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+import getRefs from './get-refs';
 
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-  searchCountry: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
-};
+const refs = getRefs();
 
-refs.searchCountry.addEventListener(
-  'input',
-  debounce(onSearch, DEBOUNCE_DELAY)
-);
+refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
-  console.log(refs.searchCountry.value);
+  const searchCountry = refs.searchBox.value.trim();
+  if (!searchCountry) return;
+  console.log(refs.searchBox.value);
+
+  fetchCountries(searchCountry)
+    .then(renderCountry)
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
+}
+
+function renderCountry(country) {
+  if (country.length > 10)
+    Notify.info('Too many matches found. Please enter a more specific name.');
+
+  if (country.length > 1 && country.length <= 10) markupCounryList(country);
+
+  if (country.length === 1) markupCounryInfo(country);
+}
+
+function markupCounryList(countres) {
+  const markupList = countres
+    .map(
+      ({ name, flags }) =>
+        `<li class = "country-item">
+            <img src = ${flags.svg} alt = "flag" class = "flag--small">
+            <h2 name = "country">${name.official}</h2>
+        </li>`
+    )
+    .join('');
+
+  refs.countryList.innerHTML = markupList;
+}
+
+function markupCounryInfo(countres) {
+  const markupInfo = countres;
 }
